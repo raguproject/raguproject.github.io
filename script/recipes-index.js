1a5436b46a0fe3ca300e6e701bb1135bf72b5741
giulia manganelli <giuliamanganelli@me.com>
2023-04-05 00:31:19 +0200

    breadcrumbs + tooltip modified

--- script/recipes-index.js
+++ script/recipes-index.js
@@ -8,7 +8,6 @@ fetch("/json/alphabet.json")
         for (const i in alphabet_json) {
             const links = alphabet_json[i];
 
-            //console.log(Object.values(links).sort());
             const div = document.getElementById('letter_space');
             const ol = document.createElement('ol');
             ol.classList = "index_letters";
@@ -178,21 +177,12 @@ fetch("/json/categories.json")
                 selectedFilters[this.name].push(this.value);
             });
 
-            // create a collection containing all of the filterable elements
             var $filteredResults = $('.recipe_a');
-            // loop over the selected filter name -> (array) values pairs
             $.each(selectedFilters, function (name, filterValues) {
                 $filteredResults = $filteredResults.filter(function () {
                     var matched = false,
                         currentFilterValues = $(this).data("allfilters").split(' ');
-
-                    // loop over each category value in the current .flower's data-category
                     $.each(currentFilterValues, function (_, currentFilterValue) {
-
-
-                        // if the current category exists in the selected filters array
-                        // set matched to true, and stop looping. as we're ORing in each
-                        // set of filters, we only need to match once
 
                         if ($.inArray(currentFilterValue, filterValues) != -1) {
                             matched = true;
@@ -200,48 +190,106 @@ fetch("/json/categories.json")
                         }
                     });
                     return matched;
-
                 });
             });
 
             $('.recipe_a').hide().filter($filteredResults).show();
             var x = $('.recipe_a').hide().filter($filteredResults).show();
+            var arr_filt = [];
+
+            $("input[type=checkbox]:checked").each(function () {
+                arr_filt.push($(this).val());
+            });
 
             $("#btn-compactlist").click(function () {
                 $("#compact-ul").html("");
-                var $this = $(this);
 
+                var $this = $(this);
                 if ($this.hasClass('btn-viewfilters')) {
                     for (var y of x) {
+                        var filt = (y.dataset.allfilters);
+                        var filt_cat = (y.dataset.category);
+                        var filt_plc = (y.dataset.place);
+                        var filt_ing = (y.dataset.ingredients);
+                        var filt_cat_split = filt_cat.split(" ");
+                        var filt_plc_split = filt_plc.split(" ");
+                        var filt_ing_split = filt_ing.split(" ");
+                        var filt_comp = filt.split(" ");
+                        const intersection_ing = arr_filt.filter(element => filt_ing_split.includes(element));
+                        const intersection_cat = arr_filt.filter(element => filt_cat_split.includes(element));
+                        const intersection_plc = arr_filt.filter(element => filt_plc_split.includes(element));
+                        const intersection = arr_filt.filter(element => filt_comp.includes(element));
                         const clone = y.cloneNode(true);
                         var list_rec = $("<li></li>").append(clone);
+                        var icon_list = $("<a></a>");
+                        var icon = $("<i></i>");
+                        $(icon_list).attr('href', '#');
+                        $(icon_list).attr('data-toggle', 'tooltip');
+
+                        if (intersection_plc == 0 && intersection_cat == 0 && intersection_ing != 0) {
+                            $(icon_list).attr('title', "The recipe contains [" + intersection_ing.join(" and ") + "].");
+                        }
+
+                        else if (intersection_plc != 0 && intersection_cat == 0 && intersection_ing == 0) {
+                            $(icon_list).attr('title', "The recipe is from [" + intersection_plc + "].");
+                        }
+
+                        else if (intersection_plc == 0 && intersection_cat != 0 && intersection_ing == 0) {
+                            $(icon_list).attr('title', "The recipe is a [" + intersection_cat + "].");
+                        }
+
+                        else if (intersection_plc != 0 && intersection_cat != 0 && intersection_ing == 0) {
+                            $(icon_list).attr('title', "The recipe is from [" + intersection_plc + "]" + " and it is a [" + intersection_cat + "].");
+                        }
+
+                        else if (intersection_plc != 0 && intersection_cat == 0 && intersection_ing != 0) {
+                            $(icon_list).attr('title', "The recipe is from [" + intersection_plc + "]" + " and contains [" + intersection_ing.join(" and ") + "].");
+                        }
+                        else if (intersection_plc == 0 && intersection_cat != 0 && intersection_ing != 0) {
+                            $(icon_list).attr('title', "The recipe contains [" + intersection_ing.join(" and ") + "]" + " and it is a [" + intersection_cat + "].");
+                        }
+
+                        else {
+                            $(icon_list).attr('title', "This recipe is from [" + intersection_plc + "]" + " , contains [" + intersection_ing.join(" and ") + "]" + " and it is a [" + intersection_cat + "].");
+                        }
+                        $(icon).attr('class', "fa-regular fa-circle-question icon-list");
                         $(list_rec).attr('id', 'list-recipes-filter');
+                        $(icon_list).append(icon);
+                        $(list_rec).append(icon_list);
                         $("#compact-ul").append(list_rec);
                     }
                 }
 
                 else if ($this.hasClass('btn-compactlist')) {
                     $("#compact-ul").html("");
+
                 }
             })
 
 
+
+
+            $(document).ready(function () {
+                $('[data-toggle="tooltip"]').tooltip();
+            });
+
             var plc = selectedFilters.place;
             var ingred = selectedFilters.ingredients;
             var cat = selectedFilters.category;
-
             $("#btn-compactlist").click(function () {
                 var $this = $(this);
 
                 if ($this.hasClass('btn-viewfilters')) {
+
                     if (plc != undefined) {
                         $(`#b-place-span`).html(plc.join(" or "));
+
                     }
                     else {
                         $(`#b-place-span`).html("no place specified");
                     }
                     if (ingred != undefined) {
-                        $(`#b-ingr-span`).html(ingred.join(" or "));
+                        $(`#b-ingr-span`).html(ingred.join(" or/and "));
                     }
                     else {
                         $(`#b-ingr-span`).html("no ingredient specified");
@@ -260,6 +308,9 @@ fetch("/json/categories.json")
 
                 }
             })
+
+
+
 
 
             /*
@@ -461,9 +512,6 @@ fetch("/json/categories.json")
 
             } else {
                 el = $(".filter-item").detach();
-                /*var h3_txt = $("<h3></h3>").text("your filtered recipes");
-                $("#compact-list").append(h3_txt);
-                $(h3_txt).attr('id', 'h3-compactlist');*/
                 $('#filter-sidebar-heading').html("filtered recipes");
                 $("#filter-sidebar").css("background-color", "#e9dfe1");
             }
